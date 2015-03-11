d033fe964341fa9add5acca839750128df0f4165
Nathan Fritz <fritzy@netflint.net>
2015-03-10 17:05:12 -0700

    basic test of query casting now passing

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var verymodel = require('verymodel');
 var baseExtensions = require('./lib/base');
 var setupExtensions = require('./lib/setup');
 var pg = require('pg');
+var util = require('util');
 
 var default_db = null;
 var default_db_uri = null;
+
+var model_cache = {};
+var model_by_type = {};
+var model_by_id = {};
 
 function Model() {
     verymodel.VeryModel.apply(this, arguments);
@@ -18,8 +23,15 @@ function Model() {
         }
     });
 
+    model_cache[this.options.table] = this;
+
     baseExtensions(this);
     setupExtensions(this);
+
+    this.register = function () {
+        model_by_type[util.format("%d->%d", this.options.tableID, this.options.columnID)] = this;
+        model_by_id[this.options.tableID] = this;
+    };
 
 }
 
@@ -35,6 +47,42 @@ module.exports = {
             default_db = conn;
             callback(err);
         });
-    }
+    },
+
+    query: function () {
+        var args = Array.prototype.splice.call(arguments, 0);
+        var callback = args.pop();
+        var cols = [];
+        var ids = {};
+        args.push(function (err, result) {
+            if (err) {
+                callback(err, result);
+                return;
+            }
+            result.fields.forEach(function (field) {
+                var cache = util.format("%d->%d", field.tableID, field.columnID);
+                if (model_by_type.hasOwnProperty(cache)) {
+                    cols.push([field.name, model_by_type[cache], field.tableID]);
+                } else if (model_by_id.hasOwnProperty(field.tableID) && model_by_id[field.tableID].options.primaryColumnID === field.columnID) {
+                    ids[field.tableID] = field.name;
+                }
+            });
+            if (cols.length === 0) {
+                callback(err, result);
+                return;
+            }
+            result.rows.forEach(function (row) {
+                cols.forEach(function (col_trans) {
+                    var inst = col_trans[1].create(row[col_trans[0]]);
+                    if (ids.hasOwnProperty(col_trans[2])) {
+                        inst.id = row[ids[col_trans[2]]];
+                    }
+                    row[col_trans[0]] = inst;
+                });
+            });
+            callback(err, result);
+        });
+        default_db.query.apply(default_db, args);
+    },
 
 };
--- test.js
+++ test.js
@@ -10,15 +10,24 @@ var TM = new PostHole.Model({
 }, {table: 'testing'});
 
 var tm = TM.create({
-    test: 'this',
+    test: 'this32',
     crap: 'that',
 });
     TM.setup(function (err) {
         tm.save(function (err) {
-            console.log(err, tm.id);
+            //console.log(err, tm.id);
             TM.load(tm.id, function (err, tm2) {
-                console.log(err);
-                console.log(tm2.toJSON(), tm2.id);
+                PostHole.query("SELECT id, value FROM testing WHERE value @> $1",[{test: 'this32'}], function (err, result) {
+                    console.log(result.rows[0].value.test, result.rows[0].value.id);
+                    console.log(result.rows.length);
+                    var inst = result.rows[0].value;
+                    inst.test = 'changed!';
+                    inst.save(function (err) {
+                        PostHole.query("SELECT id, value FROM testing WHERE id = $1", [inst.id], function (err, result) {
+                            console.log("now test is", result.rows[0].value.test);
+                        });
+                    });
+                });
             });
         });
     });
